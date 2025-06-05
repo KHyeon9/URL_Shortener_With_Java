82f4c9cd5f77e5c9744489b369b915172ea36eb2
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-06-05 14:51:15 +0900

    [Service] ShortUrlService 생성 및 public short url들을 조회하는 서비스 로직 작성

---
+++ src/main/java/com/hyeon/url_shortener/service/ShortUrlService.java
@@ -0,0 +1,22 @@
+package com.hyeon.url_shortener.service;
+
+import com.hyeon.url_shortener.domain.entity.ShortUrl;
+import com.hyeon.url_shortener.repository.ShortUrlRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShortUrlService {
+
+    private final ShortUrlRepository shortUrlRepository;
+
+    public ShortUrlService(ShortUrlRepository shortUrlRepository) {
+        this.shortUrlRepository = shortUrlRepository;
+    }
+
+    // private가 아닌 short url들을 모두 조회
+    public List<ShortUrl> findAllPublicShortUrls() {
+        return shortUrlRepository.findPublicShortUrls();
+    }
+}
