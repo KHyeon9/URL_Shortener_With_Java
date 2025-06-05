3c7d1d67af25c73c7493af9782a04b7a8df31088
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-06-05 14:52:09 +0900

    [Controller] 사용하지 않는 부분 제거 및 service 로직을 통해서 view로 public short urls 반환 및 base url 반환

---
+++ src/main/java/com/hyeon/url_shortener/web/controller/HomeContriller.java
@@ -0,0 +1,28 @@
+package com.hyeon.url_shortener.web.controller;
+
+import com.hyeon.url_shortener.domain.entity.ShortUrl;
+import com.hyeon.url_shortener.service.ShortUrlService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeContriller {
+
+    private final ShortUrlService shortUrlService;
+
+    public HomeContriller(ShortUrlService shortUrlService) {
+        this.shortUrlService = shortUrlService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<ShortUrl> shortUrls = shortUrlService.findAllPublicShortUrls();
+        model.addAttribute("shortUrls", shortUrls);
+        model.addAttribute("baseUrl", "http://localhost:8080");
+        return "index";
+    }
+
+}
