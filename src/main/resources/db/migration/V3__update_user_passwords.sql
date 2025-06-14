2851549cba0bb3fdf1a3f44d70149a9e4157bdc1
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-06-14 16:59:09 +0900

    [SQL] PasswordEncoder 추가로 인한 유저 비밀번호 변경

---
+++ src/main/resources/db/migration/V3__update_user_passwords.sql
@@ -0,0 +1,2 @@
+update users set password='$2a$10$PNdjoKw6BT0LYKPzB2FO/OBLzLyPpsAuAcU600NmRxy31Vo9kPDrq' where email='admin@gmail.com';
+update users set password='$2a$10$aZnkcgpDOipDNX.oLEzjfu4GmpMv3V0r4cHIa9591WZYAEnuV4yDC' where email='hyeon@gmail.com';
