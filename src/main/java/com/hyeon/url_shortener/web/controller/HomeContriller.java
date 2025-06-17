f312f6d317b54cd2599aeee9da532bb5843fdbc7
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-06-18 00:11:20 +0900

    [Controller] Pagenation을 Controller에 적용 및 중복된 부분 분리

--- src/main/java/com/hyeon/url_shortener/web/controller/HomeContriller.java
+++ src/main/java/com/hyeon/url_shortener/web/controller/HomeContriller.java
@@ -3,6 +3,7 @@ package com.hyeon.url_shortener.web.controller;
 import com.hyeon.url_shortener.ApplicationProperties;
 import com.hyeon.url_shortener.domain.exception.ShortUrlNotFoundException;
 import com.hyeon.url_shortener.domain.model.CreateShortUrlCmd;
+import com.hyeon.url_shortener.domain.model.PagedResult;
 import com.hyeon.url_shortener.domain.model.ShortUrlDto;
 import com.hyeon.url_shortener.web.dto.CreateShortUrlForm;
 import com.hyeon.url_shortener.service.ShortUrlService;
@@ -10,10 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -32,23 +30,23 @@ public class HomeContriller {
         this.properties = properties;
     }
 
-    // 홈 화면으로 이동
-    @GetMapping("/")
-    public String home(Model model) {
-        securityUtils.getCurrentUser();
-
-        List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
-        model.addAttribute("shortUrls", shortUrls);
-        model.addAttribute("baseUrl", properties.baseUrl());
-        model.addAttribute("createShortUrlForm",
-                new CreateShortUrlForm("", false, null));
-        return "index";
-    }
-
     // 로그인 페이지
     @GetMapping("/login")
     String loginForm() {
         return "login";
+    }
+
+    // 홈 화면으로 이동
+    // Pageable로도 가능하지만 requestParam으로 만들어봄
+    @GetMapping("/")
+    public String home(
+            @RequestParam(defaultValue = "1") Integer page,
+            Model model
+    ) {
+        this.addShortUrlDataToModel(model, page);
+        model.addAttribute("createShortUrlForm",
+                new CreateShortUrlForm("", false, null));
+        return "index";
     }
 
     // short url 생성
@@ -62,9 +60,8 @@ public class HomeContriller {
         // 오류가 있는지 확인
         if (bindingResult.hasErrors()) {
             // index 페이지에 필요한 값들을 설정
-            List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
-            model.addAttribute("shortUrls", shortUrls);
-            model.addAttribute("baseUrl", properties.baseUrl());
+            // 생성 후 1페이지부터 보여줄 것이기 때문에 1로 지정
+            this.addShortUrlDataToModel(model, 1);
             return "index";
         }
 
@@ -76,6 +73,7 @@ public class HomeContriller {
                     form.expirationInDays(),
                     userId
             );
+            // short url 생성 후 결과 가져옴
             ShortUrlDto shortUrlDto = shortUrlService.createShortUrl(cmd);
             redirectAttributes.addFlashAttribute(
                     "successMessage", "Short Url이 정상적으로 만들어 졌습니다. " +
@@ -87,8 +85,10 @@ public class HomeContriller {
         return "redirect:/";
     }
 
+    // short url을 가지고 변환전 주소로 리다이렉트
     @GetMapping("/s/{shortKey}")
     String redirectToOriginalUrl(@PathVariable("shortKey") String shortKey) {
+        // 리다이렉트될 주소 조회
         Long userId = securityUtils.getCurrentUserId();
         Optional<ShortUrlDto> shortUrlDtoOptional = shortUrlService.accessShortUrl(shortKey, userId);
 
@@ -97,7 +97,16 @@ public class HomeContriller {
         }
 
         ShortUrlDto shortUrlDto = shortUrlDtoOptional.get();
-
         return "redirect:" + shortUrlDto.originalUrl();
+    }
+
+    // 중복되는 부분을 분리
+    private void addShortUrlDataToModel(Model model, int pageNo) {
+        PagedResult<ShortUrlDto> shortUrls = shortUrlService
+                .findAllPublicShortUrls(pageNo, properties.pageSize());
+
+        // View로 데이터 전송
+        model.addAttribute("shortUrls", shortUrls);
+        model.addAttribute("baseUrl", properties.baseUrl());
     }
 }
