d2e5820de69f9d6da88e092dc5424f1db0a82f80
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-06-22 19:07:36 +0900

    [Repository] 이메일 확인 메소드 추가

--- src/main/java/com/hyeon/url_shortener/repository/UserRepository.java
+++ src/main/java/com/hyeon/url_shortener/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.hyeon.url_shortener.repository;
 
 import com.hyeon.url_shortener.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByEmail(String email);
+    // 이메일로 user 찾기
+    Optional<User> findByEmail(String email);
+    // 이메일이 있는지 확인
+    boolean existsByEmail(String email);
 }
